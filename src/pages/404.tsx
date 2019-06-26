e990a98e60bd6439f1efda19c19fdc6c8b0218d4
Nilkanth Patel <nilkanth@midtype.com>
2019-06-26 13:50:16 -0700

    Starter files for template

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Layout from '../layout/Layout';
+
+const NotFoundPage = () => (
+  <Layout>
+    <h1>404!</h1>
+  </Layout>
+);
+
+export default NotFoundPage;
